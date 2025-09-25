02ce39fa06e1fef764a5c28a9f571f2c29fd4669
ArthP28 <apisanko@carthage.edu>
2025-09-24 19:27:00 -0500

    Reconfigured Player class to Struct
    Helps with organization since the only attribute of each player is the checker symbol, and cannot do anything but place their checkers on the game board.

--- code/Board.h
+++ code/Board.h
@@ -1,24 +1,34 @@
 #pragma once
+#include <vector>
 
 using namespace std;
 
 class Board{
+    struct Player{
+        char symbol;
+    };
+
     public:
-        Board();
+        Board(char p1Symbol, char p2Symbol);
         bool placeChecker(int playerNum, int columnToPlace);
-        void checkWin();
+        void checkWin(int checkerNum);
         void printBoard();
-    protected:
+
+    private:
+        Player _players[2];
         int _boardSpaces[7][6];
 };
+    
 
-Board::Board(){
+Board::Board(char p1Symbol, char p2Symbol){
     // Make sure that entire board starts completely empty (Full of 0s)
     for(int col = 0; col < 7; col++){
         for(int row = 0; row < 6; row++){
             _boardSpaces[col][row] = 0;
         }
     }
+    _players[0].symbol = p1Symbol;
+    _players[1].symbol = p2Symbol;
 }
 
 bool Board::placeChecker(int playerNum, int columnToPlace){
@@ -39,7 +49,7 @@ bool Board::placeChecker(int playerNum, int columnToPlace){
     return canBePlaced;
 }
 
-void Board::checkWin(){
+void Board::checkWin(int checkerNum){
     // Scan the entire board for four in a row after each turn
     // 1. Horizontals
     // 2. Verticals
@@ -49,7 +59,13 @@ void Board::checkWin(){
 void Board::printBoard(){
     for(int row = 0; row < 6; row++){
         for(int col = 0; col < 7; col++){
-            cout << _boardSpaces[col][row] << " ";
+            if(_boardSpaces[col][row] == 1){ // Player 1 Checkers
+                cout << _players[0].symbol << " ";
+            } else if(_boardSpaces[col][row] == 2){ // Player 2 Checkers
+                cout << _players[1].symbol << " ";
+            } else { // Nothing
+                cout << _boardSpaces[col][row] << " ";
+            }
         }
         cout << endl;
     }
--- code/main.cpp
+++ code/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    Board _board;
+    Board _board('X', 'Y');
     _board.placeChecker(1, 4);
     _board.placeChecker(2, 4);
     _board.placeChecker(1, 4);

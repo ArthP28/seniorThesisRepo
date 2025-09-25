5631075c7da790642e13fbd1f676fac4799a106e
ArthP28 <apisanko@carthage.edu>
2025-09-24 20:33:43 -0500

    Winning Algorithm Implemented
    Linear Search algorithm that looks for winning lines horizontally, vertically, and diagonally.

--- code/Board.h
+++ code/Board.h
@@ -11,7 +11,7 @@ class Board{
     public:
         Board(char p1Symbol, char p2Symbol);
         bool placeChecker(int playerNum, int columnToPlace);
-        void checkWin(int checkerNum);
+        bool checkWin(int checkerNum);
         void printBoard();
 
     private:
@@ -49,11 +49,97 @@ bool Board::placeChecker(int playerNum, int columnToPlace){
     return canBePlaced;
 }
 
-void Board::checkWin(int checkerNum){
+// FUTURE TODO: In > 4-in-a-row, Current Win Algorithm searches through checkers it has already searched
+// If possible, find a way to ignore already searched checker rows
+bool Board::checkWin(int checkerNum){
+    bool hasWon = false;
+    int connectCount = 0;
     // Scan the entire board for four in a row after each turn
-    // 1. Horizontals
-    // 2. Verticals
-    // 3. Diagonals
+    for(int row = 0; row < 6; row++){
+        for(int col = 0; col < 7; col++){
+            if(_boardSpaces[col][row] == checkerNum){ // When the loop finds the matching player checker, it then checks to see if there is another
+                // Win searching algorithms check if there is four in a row
+                // If there is, winning bool is set to true
+                // If not, reset the connectCounter and go to the other search algorithm
+                if(_boardSpaces[col+1][row] == checkerNum) { // 1. Horizontaly
+                    connectCount+=2; // Two player checkers found so far
+                    for(int c1 = 2; c1 < 4; c1++){
+                        if(_boardSpaces[col + c1][row] == checkerNum){
+                            connectCount++;
+                        } else {
+                            break;
+                        }
+                    }
+                    if(connectCount == 4){
+                        cout << "Connect Four!" << endl;
+                        hasWon = true;
+                        break;
+                    } else {
+                        connectCount = 0;
+                    }
+                }
+
+                if(_boardSpaces[col][row+1] == checkerNum) { // 2. Vertically
+                    connectCount+=2;
+                    for(int r1 = 2; r1 < 4; r1++){
+                        if(_boardSpaces[col][row + r1] == checkerNum){
+                            connectCount++;
+                        } else {
+                            break;
+                        }
+                    }
+                    if(connectCount == 4){
+                        cout << "Connect Four!" << endl;
+                        hasWon = true;
+                        break;
+                    } else {
+                        connectCount = 0;
+                    }
+                }
+
+                if(_boardSpaces[col-1][row+1] == checkerNum) { // 3. Diagonally (Left)
+                    connectCount+=2;
+                    for(int d = 2; d < 4; d++){
+                        if(_boardSpaces[col - d][row + d] == checkerNum){ // Searching left and down
+                            connectCount++;
+                        } else {
+                            break;
+                        }
+                    }
+                    if(connectCount == 4){
+                        cout << "Connect Four!" << endl;
+                        hasWon = true;
+                        break;
+                    } else {
+                        connectCount = 0;
+                    }
+                }
+
+                if(_boardSpaces[col+1][row+1] == checkerNum) { // 3. Diagonally (Right)
+                    connectCount+=2;
+                    for(int d = 2; d < 4; d++){
+                        if(_boardSpaces[col + d][row + d] == checkerNum){ // Searching right and down
+                            connectCount++;
+                        } else {
+                            break;
+                        }
+                    }
+                    if(connectCount == 4){
+                        cout << "Connect Four!" << endl;
+                        hasWon = true;
+                        break;
+                    } else {
+                        connectCount = 0;
+                    }
+                }
+            }
+            if(hasWon){ // If there is a connect four, there is no further need to keep searching
+                break;
+            }
+        }
+    }
+    
+    return hasWon;
 }
 
 void Board::printBoard(){
--- code/main.cpp
+++ code/main.cpp
@@ -5,14 +5,26 @@ using namespace std;
 int main()
 {
     Board _board('X', 'Y');
-    _board.placeChecker(1, 4);
+    _board.placeChecker(1, 5);
     _board.placeChecker(2, 4);
+    _board.placeChecker(2, 3);
+    _board.placeChecker(2, 2);
+
     _board.placeChecker(1, 4);
-    _board.placeChecker(2, 4);
-    _board.placeChecker(1, 4);
-    _board.placeChecker(2, 4);
+    _board.placeChecker(2, 3);
+    _board.placeChecker(2, 2);
+
     _board.placeChecker(1, 3);
+    _board.placeChecker(2, 2);
+    _board.placeChecker(1, 2);
+
     _board.printBoard();
+    if(_board.checkWin(1)){
+        cout << "Player 1 Wins!" << endl;
+    }
+    if(_board.checkWin(2)){
+        cout << "Player 2 Wins!" << endl;
+    }
 
     return 0;
 }
